bedddf2bca79382a83e170ec047d846c33b63d00
Anubhav <choubey.anubhav253@gmail.com>
2024-08-13 20:14:02 +0530

    Added Card to display information

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("HELLO HOME"));
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 57,
+              child: Card(
+                color: const Color.fromARGB(255, 207, 203, 214),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Welcome Anubhav",
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Text(
+                        "Class X Std A, Roll No. 14",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }  
