7baf0133329b391231f89d8fbb4801898ec8edfb
Anubhav <choubey.anubhav253@gmail.com>
2024-08-13 19:48:14 +0530

    Changed name to home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
