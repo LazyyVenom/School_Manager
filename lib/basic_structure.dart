04a5c3718b74a9b7133b75401c2f0f6300697da6
Anubhav <choubey.anubhav253@gmail.com>
2024-08-13 19:44:53 +0530

    Added Nav bar navigation now need to add Pages accourdingly

--- lib/basic_structure.dart
+++ lib/basic_structure.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
-class BasicStructure extends StatelessWidget {
+class BasicStructure extends StatefulWidget {
   const BasicStructure({
     super.key,
   });
+
+  @override
+  State<BasicStructure> createState() => _BasicStructureState();
+}
+
+class _BasicStructureState extends State<BasicStructure> {
+  int currentPage = 0;
+  List<Widget> pages = const [];
+
+  @override
+  void initState() {
+    currentPage = 0;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +26,21 @@ class BasicStructure extends StatelessWidget {
         title: const Text("School_Name"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Hello World"),
+      body: IndexedStack(
+        index: currentPage,
+        children: pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
         selectedFontSize: 0,
         unselectedFontSize: 0,
+        currentIndex: currentPage,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
         iconSize: 26,
         items: const [
           BottomNavigationBarItem(
