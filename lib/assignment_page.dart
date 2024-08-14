84f98ef588a95613a70ba10db2b97a26ae0d3f39
Anubhav <choubey.anubhav253@gmail.com>
2024-08-14 13:03:54 +0530

    Need to add attendance calender

--- lib/assignment_page.dart
+++ lib/assignment_page.dart
@@ -1,7 +1,15 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-class AssignmentPage extends StatelessWidget {
+class AssignmentPage extends StatefulWidget {
   const AssignmentPage({super.key});
+
+  @override
+  State<AssignmentPage> createState() => _AssignmentPageState();
+}
+
+class _AssignmentPageState extends State<AssignmentPage> {
+  final List<String> categories = ['All', 'Assignments', 'Homework'];
+  String selectedCategory = 'All';
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +19,36 @@ class AssignmentPage extends StatelessWidget {
         children: [
           const SizedBox(height: 6),
           Text(
-            "Assignment & HomeWork",
+            "Assignments",
             style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(height: 6),
+          const Divider(),
+          const SizedBox(height: 6),
+          SizedBox(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedCategory = categories[index];
+                      });
+                    },
+                    child: Chip(
+                      label: Text(categories[index]),
+                      backgroundColor: selectedCategory == categories[index]
+                          ? Colors.deepPurple[200]
+                          : Colors.deepPurple[50],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
           const SizedBox(height: 6),
           const Divider(),
@@ -23,30 +59,28 @@ class AssignmentPage extends StatelessWidget {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.all(5.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(1),
-                        color: Colors.deepPurple[50],
-                        child: ListTile(
-                          leading: Icon(Icons.book, color: Colors.deepPurple[300],),
-                          title: Text("Home Work for Grade $index "),
-                          subtitle:
-                              Text("Sent On: Regarding Office Work for $index"),
-                          trailing: Icon(
-                            Icons.arrow_circle_right,
-                            color: Colors.deepPurple[300],
-                          ),
-                        ),
-                      )
-                    ],
+                  child: Container(
+                    padding: const EdgeInsets.all(1),
+                    color: Colors.deepPurple[50],
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.bookmark,
+                        color: Colors.deepPurple[300],
+                      ),
+                      title: Text("Homework for Grade $index"),
+                      subtitle: Text("Sent On: Regarding Office Work for $index"),
+                      trailing: Icon(
+                        Icons.arrow_circle_right,
+                        color: Colors.deepPurple[300],
+                      ),
+                    ),
                   ),
                 );
               },
             ),
-          )
+          ),
         ],
       ),
     );
   }
-}  
+}
