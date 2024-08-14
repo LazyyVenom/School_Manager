92eb69def66d9603326d2af53b4d0ed7b17a2e4d
Anubhav <choubey.anubhav253@gmail.com>
2024-08-14 12:36:57 +0530

    Added Assignments Page

--- lib/assignment_page.dart
+++ lib/assignment_page.dart
@@ -5,6 +5,48 @@ class AssignmentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("HELLO Assignment"));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          const SizedBox(height: 6),
+          Text(
+            "Assignment & HomeWork",
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(height: 6),
+          const Divider(),
+          const SizedBox(height: 6),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(1),
+                        color: Colors.deepPurple[50],
+                        child: ListTile(
+                          leading: Icon(Icons.book, color: Colors.deepPurple[300],),
+                          title: Text("Home Work for Grade $index "),
+                          subtitle:
+                              Text("Sent On: Regarding Office Work for $index"),
+                          trailing: Icon(
+                            Icons.arrow_circle_right,
+                            color: Colors.deepPurple[300],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }  
