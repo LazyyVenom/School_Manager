f6c7da57ae312e72c1ab399bceea00a963fd24e6
Anubhav <choubey.anubhav253@gmail.com>
2024-08-15 11:37:38 +0530

    Added Basic Calender

--- lib/attendance_page.dart
+++ lib/attendance_page.dart
@@ -1,14 +1,26 @@
 import "package:flutter/material.dart";
+import "package:table_calendar/table_calendar.dart";
 
 class AttendancePage extends StatelessWidget {
   const AttendancePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
-        
+        children: [
+          Text(
+            "Attendance",
+            style: Theme.of(context).textTheme.displayMedium,
+            ),
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: DateTime.now(),
+          ),
+          
+        ],
       ),
     );
   }
