a2064c714d1db092bfa80e3a567809e9d805599b
Anubhav <choubey.anubhav253@gmail.com>
2024-08-17 15:45:13 +0530

    Added Gitignore

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -92,33 +92,7 @@ class HomePage extends StatelessWidget {
             const SizedBox(height: 6),
             const Divider(),
             const SizedBox(height: 6),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 15,
-                itemBuilder:(context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(1),
-                          color: Colors.deepPurple[50],
-                          child: ListTile(
-                            leading: index % 3 == 0 ? Icon(Icons.book, color: Colors.deepPurple[300],) : Icon(Icons.add_alert, color: Colors.deepPurple[300],),
-                            title: Text("Home Work for Grade $index "),
-                            subtitle: Text("Sent On: Regarding Office Work for $index"),
-                            trailing: Icon(
-                              Icons.arrow_circle_right,
-                              color: Colors.deepPurple[300],
-                              ),
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
+            
           ],
         ),
       ),
