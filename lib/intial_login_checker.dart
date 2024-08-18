38cbfeb73548688f449202b3a26aabee23cc702b
Anubhav <choubey.anubhav253@gmail.com>
2024-08-18 21:40:08 +0530

    Adding login feature

---
+++ lib/intial_login_checker.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InitialChecker extends StatelessWidget {
+  const InitialChecker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
