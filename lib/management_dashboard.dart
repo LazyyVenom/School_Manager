9c5b088d2daeed52758867f6ad7ade3b8048bf67
Anubhav <choubey.anubhav253@gmail.com>
2024-08-25 12:51:38 +0530

    Added Basic dashboard will display on login

---
+++ lib/management_dashboard.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:school_manager/alert_page.dart';
+import 'package:school_manager/assignment_page.dart';
+import 'package:school_manager/attendance_page.dart';
+import 'package:school_manager/bus_tracking_page.dart';
+import 'package:school_manager/home_page.dart';
+
+class StudentDashboard extends StatefulWidget {
+  const StudentDashboard({
+    super.key,
+  });
+
+  @override
+  State<StudentDashboard> createState() => _BasicStructureState();
+}
+
+class _BasicStructureState extends State<StudentDashboard> {
+  int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    AlertPage(),
+    BusTrackingPage(),
+    AssignmentPage(),
+    AttendancePage()
+  ];
+
+  @override
+  void initState() {
+    currentPage = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("School_Name"),
+        centerTitle: true,
+      ),
+      body: IndexedStack(
+        index: currentPage,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.deepPurple[100],
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        currentIndex: currentPage,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        iconSize: 26,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+            backgroundColor: Colors.deepPurple
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.warning),
+            label: "Alert",
+            backgroundColor: Colors.deepPurple
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bus_alert),
+            label: "Bus Tracking",
+            backgroundColor: Colors.deepPurple
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: "Assignment",
+            backgroundColor: Colors.deepPurple
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Attendance",
+            backgroundColor: Colors.deepPurple
+          ),
+        ],
+      ),
+    );
+  }
+}
