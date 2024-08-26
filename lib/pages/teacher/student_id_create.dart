ef5943a8c239bf36dc5512802e55fc757ef5f637
Anubhav <choubey.anubhav253@gmail.com>
2024-08-26 12:59:54 +0530

    Added Pages for teacher

---
+++ lib/pages/teacher/student_id_create.dart
@@ -0,0 +1,176 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firebase Firestore package
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:school_manager/auth/auth_service.dart';
+
+class TeacherRegisterPage extends StatefulWidget {
+  const TeacherRegisterPage({super.key});
+
+  @override
+  State<TeacherRegisterPage> createState() => _TeacherRegisterPageState();
+}
+
+class _TeacherRegisterPageState extends State<TeacherRegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _classController = TextEditingController();
+  final TextEditingController _sectionController = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register Teacher"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 50.0),
+            const Text(
+              "Register New Teacher",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _classController,
+              decoration: const InputDecoration(
+                labelText: "Class",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _sectionController,
+              decoration: const InputDecoration(
+                labelText: "Section",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _isLoading ? null : _registerTeacher,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: const TextStyle(fontSize: 18),
+                ),
+                child: _isLoading
+                    ? SizedBox(
+                        width: 24,
+                        height: 24,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2.0,
+                          color: Colors.deepPurple[200],
+                        ),
+                      )
+                    : const Text("Register"),
+              ),
+            ),
+            const SizedBox(height: 50.0),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _registerTeacher() async {
+    // Dismiss the keyboard
+    FocusScope.of(context).unfocus();
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    final authService = Provider.of<AuthService>(context, listen: false);
+
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+    String name = _nameController.text.trim();
+    String classAssigned = _classController.text.trim();
+    String section = _sectionController.text.trim();
+
+    try {
+      // Register the teacher using Firebase Authentication
+      final user = await authService.registerWithEmailAndPassword(email, password);
+
+      // Store additional teacher details in Firestore using email as the UID
+      await FirebaseFirestore.instance.collection('users').doc(user!.uid).set({
+        'email': email,
+        'name': name,
+        'role': 'teacher',
+        'class': classAssigned,
+        'section': section,
+      });
+
+      // Show a success message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text(
+            "Teacher Registered Successfully!",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.green[200],
+        ),
+      );
+
+      // clear the fields
+      _emailController.clear();
+      _nameController.clear();
+      _classController.clear();
+      _sectionController.clear();
+      _passwordController.clear();
+
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.toString(),
+            style: const TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.red[200],
+        ),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+}
