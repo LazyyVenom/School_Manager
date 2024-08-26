d64e2d0cb7152c7bd1c11680441063961f7c44b5
Anubhav <choubey.anubhav253@gmail.com>
2024-08-26 13:37:17 +0530

    Add Alert Page Button to teachers

--- lib/pages/teacher/assignment_page_th.dart
+++ lib/pages/teacher/assignment_page_th.dart
@@ -15,68 +15,84 @@ class _AssignmentPageThState extends State<AssignmentPageTh> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
+      child: Stack(
         children: [
-          const SizedBox(height: 6),
-          Text(
-            "Assignments",
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          const SizedBox(height: 6),
-          const Divider(),
-          const SizedBox(height: 6),
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedCategory = categories[index];
-                      });
-                    },
-                    child: Chip(
-                      label: Text(categories[index]),
-                      backgroundColor: selectedCategory == categories[index]
-                          ? Colors.deepPurple[200]
-                          : Colors.deepPurple[50],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          const SizedBox(height: 6),
-          const Divider(),
-          const SizedBox(height: 6),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 9,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Container(
-                    padding: const EdgeInsets.all(1),
-                    color: Colors.deepPurple[50],
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.bookmark,
-                        color: Colors.deepPurple[300],
+          Column(
+            children: [
+              const SizedBox(height: 6),
+              Text(
+                "Assignments",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: 6),
+              const Divider(),
+              const SizedBox(height: 6),
+              SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedCategory = categories[index];
+                          });
+                        },
+                        child: Chip(
+                          label: Text(categories[index]),
+                          backgroundColor: selectedCategory == categories[index]
+                              ? Colors.deepPurple[200]
+                              : Colors.deepPurple[50],
+                        ),
                       ),
-                      title: Text("Homework for Grade $index"),
-                      subtitle: Text("Sent On: Regarding Office Work for $index"),
-                      trailing: Icon(
-                        Icons.arrow_circle_right,
-                        color: Colors.deepPurple[300],
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 6),
+              const Divider(),
+              const SizedBox(height: 6),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 9,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(
+                        padding: const EdgeInsets.all(1),
+                        color: Colors.deepPurple[50],
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.bookmark,
+                            color: Colors.deepPurple[300],
+                          ),
+                          title: Text("Homework for Grade $index"),
+                          subtitle:
+                              Text("Sent On: Regarding Office Work for $index"),
+                          trailing: Icon(
+                            Icons.arrow_circle_right,
+                            color: Colors.deepPurple[300],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                );
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              onPressed: () {
+                // Add your onPressed action here
               },
+              backgroundColor: Colors.deepPurple,
+              child: const Icon(Icons.add),
             ),
           ),
         ],
