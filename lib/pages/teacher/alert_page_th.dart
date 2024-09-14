6dd028f699677033d2ddd53845157e63b2be1ea2
Anubhav <choubey.anubhav253@gmail.com>
2024-09-14 17:24:54 +0530

    Fixed notification

--- lib/pages/teacher/alert_page_th.dart
+++ lib/pages/teacher/alert_page_th.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:school_manager/additional_features.dart';
 import 'package:school_manager/notification_service.dart';
